b6c5be7b41722bf723608688f513fad20945d299
Ari Lerner <arilerner@mac.com>
2016-05-06 11:27:55 -0600

    Updated rebuild of webhook

--- machines/webhook/rebuild.sh
+++ machines/webhook/rebuild.sh
@@ -7,13 +7,13 @@ NAME=$3
 REPO_PATH=/tmp/$NAME
 GIT=$(which git)
 
-(
-  $GIT clone --depth 1 $CLONE_URL $REPO_PATH
-  cd $REPO_PATH
-  $GIT reset $SHA1 --hard
 
-  ## Build the images
-  make build-all
-)
+$GIT clone --depth 1 $CLONE_URL $REPO_PATH
+pushd $REPO_PATH
+$GIT reset $SHA1 --hard
+
+## Build the images
+make build-all
+popd
 
 rm -rf /tmp/$NAME
