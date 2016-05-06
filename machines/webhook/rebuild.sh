4df9564c41742218f8841ff2bc5befad3df4acad
Ari Lerner <arilerner@mac.com>
2016-05-05 20:32:57 -0600

    Updates

--- machines/webhook/rebuild.sh
+++ machines/webhook/rebuild.sh
@@ -4,14 +4,15 @@ CLONE_URL=$1
 SHA1=$2
 NAME=$3
 
+REPO_PATH=/tmp/$NAME
 GIT=$(which git)
 
-echo "git: $GIT"
+(
+  $GIT clone --depth 1 $CLONE_URL $REPO_PATH
+  cd $REPO_PATH
+  $GIT reset $SHA1 --hard
 
-$GIT clone --depth 1 $CLONE_URL $NAME
-cd $NAME
-$GIT reset $SHA1 --hard
+  make build-all
+)
 
-pwd
-
-make build-all
+rm -rf /tmp/$NAME
