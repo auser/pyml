1132385381ce4907a1e0188a90ee17910fa6c758
Ari Lerner <arilerner@mac.com>
2016-05-16 13:18:18 -0700

    Updated theanorc

--- machines/notebook-opencv/system-conf/etc/profile.d/local.sh
+++ machines/notebook-opencv/system-conf/etc/profile.d/local.sh
@@ -1,3 +1,10 @@
 # Add ~/.local directory to system paths
 export PATH="${HOME}/.local/bin:${PATH}"
 export LD_LIBRARY_PATH="${HOME}/.local/lib:${LD_LIBRARY_PATH}:/usr/local/cuda/lib:/usr/local/cuda/lib64"
+
+# Add CUDA
+export PATH="$PATH:/usr/local/cuda/bin"
+
+echo "Liftoff!"
+
+export LOCAL_PROFILE_SETUP="true"
